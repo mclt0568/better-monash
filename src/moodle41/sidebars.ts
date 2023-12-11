e20912ac33affa49006fd1c5cd8750bac9e596ae
frankium <frankium@frankium.com>
2023-12-12 03:38:03 +1100

    some sidebar thing

--- src/moodle41/sidebars.ts
+++ src/moodle41/sidebars.ts
@@ -1,22 +1,78 @@
 import $ from "jquery";
 import arrowUp from "./assets/arrowUp.svg";
 
-function sidebarToMoodle41(){
+export function sidebarToMoodle41(){
   // side pane (to old monash)
-  if ($("#inst38662 .footer .bm-to-moodle39").length === 0){
-    $("<a></a>")
-      .attr({
-        href:"https://lms.monash.edu/"
-      })
-      .append("Moodle 3.9")
-      .append($("<img/>").attr({
-        src: arrowUp,
-      }))
-      .addClass("bm-button")
-      .addClass("bm-accent-secondary")
-      .addClass("bm-to-moodle39")
-      .appendTo("#inst38662 .footer");
+  if ($("#inst38662 .footer .bm-to-moodle39").length !== 0){
+    return;
   }
+  $("<a></a>")
+    .attr({
+      href:"https://lms.monash.edu/"
+    })
+    .append("Moodle 3.9")
+    .append($("<img/>").attr({
+      src: arrowUp,
+    }))
+    .addClass("bm-button")
+    .addClass("bm-accent-secondary")
+    .addClass("bm-to-moodle39")
+    .appendTo("#inst38662 .footer");
 }
 
-export {sidebarToMoodle41};
+export function concerningBehaviourHelp(){
+  // side pane (to old monash)
+  if ($("#inst240 .footer .bm-concerning-behaviour-help").length !== 0){
+    return;
+  }
+  const helpLink = $("#inst240 a:not([class])").attr("href");
+  
+  $("<a></a>")
+    .attr({
+      href: helpLink
+    })
+    .append("Get Help")
+    .append($("<img/>").attr({
+      src: arrowUp,
+    }))
+    .addClass("bm-button")
+    .addClass("bm-accent-secondary")
+    .addClass("bm-concerning-behaviour-help")
+    .appendTo("#inst240 .footer");
+}
+
+export function covidHelp(){
+  if ($("#inst250980 h5").length !== 0){
+    return;
+  }
+
+  $("<h5></h5>")
+    .addClass("card-title")
+    .text("About COVID-19")
+    .prependTo("#inst250980 .card-body");
+
+  const paragraphs = $("#inst250980 .card-text .no-overflow p");
+  if (paragraphs.length === 2){
+    return ;
+  }
+
+  paragraphs.eq(0).remove();
+}
+
+export function mfaHelp(){
+  if ($("#inst250979 h5").length !== 0){
+    return;
+  }
+
+  $("<h5></h5>")
+    .addClass("card-title")
+    .text("Changing your phone: Multi-factor authentication (MFA).")
+    .prependTo("#inst250979 .card-body");
+
+  const paragraphs = $("#inst250979 .card-text .no-overflow p");
+  if (paragraphs.length === 2){
+    return ;
+  }
+
+  paragraphs.eq(0).remove();
+}
--- src/moodle41/url.ts
+++ src/moodle41/url.ts
@@ -1,17 +1,36 @@
-import { sidebarToMoodle41 } from "./sidebars";
+import { 
+  sidebarToMoodle41, 
+  concerningBehaviourHelp,
+  covidHelp,
+  mfaHelp
+} from "./sidebars";
 
 type ThemeLookup = {
   urls: RegExp[],
   callbacks: (()=>void)[],
 }
 
+const urls = {
+  any: /.*/,
+  dashboard: /^https\:\/\/learning\.monash\.edu\/my(\/|\/index.php|\/index.php\/)?$/,
+  myUnits: /^https\:\/\/learning\.monash\.edu\/my\/courses.php$/,
+};
+
 const themeLookups: ThemeLookup[] = [
   {
+    urls: [urls.dashboard],
+    callbacks: [covidHelp, mfaHelp]
+  },
+  {
     urls: [
-      /^https\:\/\/learning\.monash\.edu\/my\/?$/,
-      /^https\:\/\/learning\.monash\.edu\/my\/courses.php$/,
+      urls.dashboard,
+      urls.myUnits,
     ], 
     callbacks: [sidebarToMoodle41]
+  },
+  {
+    urls: [urls.any],
+    callbacks: [concerningBehaviourHelp]
   }
 ]
 
@@ -19,7 +38,6 @@ function applyThemes(){
   for (const lookup of themeLookups){
     const matched = lookup.urls.some((pattern) => pattern.test(location.href));
     if (matched){
-      console.log("MATCHED!");
       lookup.callbacks.forEach((callback)=>callback());
     }
   }
